dfb93a8fc720f33120e518f682201aa68da06367
Thibault <thibault.laine@inria.fr>
2019-04-26 19:46:07 +0200

    first commit

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+	}
+}
