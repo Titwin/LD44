7672c0dbe3478e9f306698228e55775ce867adce
Thibault <thibault.laine@inria.fr>
2019-04-27 12:49:44 +0200

    charactercontroller class

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -1,49 +1,110 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// This script moves the character controller forward
-// and sideways based on the arrow keys.
-// It also jumps when pressing space.
-// Make sure to attach a character controller to the same game object.
-// It is recommended that you make only one call to Move or SimpleMove per frame.
+public class CharacterController2D : MonoBehaviour {
 
-public class CharacterController2D : MonoBehaviour
-{
-    CharacterController characterController;
+    private Rigidbody2D rb;
+    private Animator animator;
 
-    public float speed = 6.0f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
+    public float speed = 1;
+    public float jumpSpeed = 1.0f;
 
-    private Vector3 moveDirection = Vector3.zero;
+    public bool canAttack = true;
+    public float attackCooldownTime = 1;
+    private IEnumerator cooldownCoroutine;
 
-    void Start()
-    {
-        characterController = GetComponent<CharacterController>();
+    private float distToGround;
+
+    int idleHash;
+    int walkingHash;
+    int attackingHash;
+    int duckingHash;
+    int inAirHash;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        distToGround = GetComponent<CircleCollider2D>().bounds.extents.y + 0.1f;
+
+        int jumpHash = Animator.StringToHash("Jump");
+        int runStateHash = Animator.StringToHash("Base Layer.Run");
+
+        idleHash = Animator.StringToHash("idle");
+        walkingHash = Animator.StringToHash("walking");
+        attackingHash = Animator.StringToHash("attacking");
+        duckingHash = Animator.StringToHash("ducking");
+        inAirHash = Animator.StringToHash("inAir");
     }
-
-    void Update()
+	
+	// Update is called once per frame
+	void Update ()
     {
-        if (characterController.isGrounded)
+        animator.ResetTrigger(idleHash);
+        animator.ResetTrigger(walkingHash);
+        animator.ResetTrigger(attackingHash);
+        animator.ResetTrigger(duckingHash);
+        animator.ResetTrigger(inAirHash);
+
+        float dx = Input.GetAxis("Horizontal") * speed;
+        if (IsGrounded())
         {
-            // We are grounded, so recalculate
-            // move direction directly from axes
-
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection *= speed;
-
-            if (Input.GetButton("Jump"))
+            if(Input.GetButtonDown("Jump"))
             {
-                moveDirection.y = jumpSpeed;
+                rb.velocity = new Vector2(dx, jumpSpeed);
+                animator.SetTrigger(inAirHash);
+            }
+            else if (Input.GetButton("Duck"))
+            {
+                rb.velocity = new Vector2(dx / 2, 0);
+                animator.SetTrigger(duckingHash);
+            }
+            else
+            {
+                rb.velocity = new Vector2(dx, rb.velocity.y);
+                if(rb.velocity == new Vector2(0,0))
+                {
+                    animator.SetTrigger(idleHash);
+                }
+                else
+                {
+                    animator.SetTrigger(walkingHash);
+                }
             }
         }
+        else
+        {
+            animator.SetTrigger(inAirHash);
+            rb.velocity = new Vector2(dx, rb.velocity.y);
+        }
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
-        moveDirection.y -= gravity * Time.deltaTime;
-
-        // Move the controller
-        characterController.Move(moveDirection * Time.deltaTime);
+        if (Input.GetButtonDown("Fire1") && canAttack)
+        {
+            cooldownCoroutine = AttackCooldown(attackCooldownTime);
+            StartCoroutine(cooldownCoroutine);
+            canAttack = false;
+        }
+        if(!canAttack)
+        {
+            animator.SetTrigger(attackingHash);
+        }
     }
+    public bool IsGrounded()
+    {
+        //return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position - Vector3.up* distToGround, -Vector2.up, 0.1f);
+        if (hit.collider != null && hit.collider.gameObject!=this.gameObject)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private IEnumerator AttackCooldown(float time)
+    {
+        yield return new WaitForSeconds(time);
+        canAttack = true;
+    }
+
 }
