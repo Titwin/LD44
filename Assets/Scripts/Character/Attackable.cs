fa33e2f3aa2676bf4130a2bcd3353e5bfb4c346b
solroo <solroo@gmail.com>
2019-04-28 19:01:18 +0200

    hurt/destroy alpha animations are now an Attackable behavior

--- Assets/Scripts/Character/Attackable.cs
+++ Assets/Scripts/Character/Attackable.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Attackable: MonoBehaviour
 {
     public Health health;
+    [SerializeField] SpriteRenderer sprite;
+
+    protected virtual void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
     [SerializeField] float destroyTime = 0.1f;
     public void DoDamage(Character source, int amount)
     {
@@ -40,11 +46,49 @@ public class Attackable: MonoBehaviour
     }
     protected virtual void OnHurt(GameObject source)
     {
-
+        StartCoroutine(DoBlink());
     }
 
     protected virtual void OnDeath(GameObject source)
     {
+        StopCoroutine(DoBlink());
+        StartCoroutine(DoFade());
+    }
 
+    bool blinking = false;
+    IEnumerator DoFade()
+    {
+        if (!blinking)
+        {
+            Color initialColor = sprite.color;
+            Color targetColor = new Color(0, 0, 0, 0);
+            float t = 0;
+            while(t<destroyTime)
+            {
+                sprite.color = Color.Lerp(initialColor,targetColor,t/destroyTime);
+                yield return new WaitForEndOfFrame();
+                t += Time.deltaTime;
+            }
+            sprite.color = targetColor;
+        }
+    }
+    IEnumerator DoBlink()
+    {
+        if (!blinking)
+        {
+            blinking = true;
+            Color initialColor = sprite.color;
+            Color currentColor = initialColor;
+            float t = 0;
+            while (blinking && health.Invulnerable)
+            {
+                currentColor.a = ((int)((Mathf.Cos(t * 360 * 5f) * 0.5f + 0.5f) * 10)) / 10f;
+                sprite.color = currentColor;
+                yield return new WaitForEndOfFrame();
+                t += Time.deltaTime;
+            }
+            sprite.color = initialColor;
+            blinking = false;
+        }
     }
 }
--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -8,7 +8,6 @@ public abstract class Character : Attackable
     public Weapon weapon;
 
     [SerializeField] GameObject bodyTemplate;
-    [SerializeField] SpriteRenderer sprite;
 
     protected virtual void Start()
     {
@@ -22,7 +21,7 @@ public abstract class Character : Attackable
             Debug.LogError(name + " has no weapon");
         }
 
-        sprite = GetComponent<SpriteRenderer>();
+        
     }
 
     public bool Attack()
@@ -35,11 +34,6 @@ public abstract class Character : Attackable
         else { return false; }
     }
 
-    protected override void OnHurt(GameObject source)
-    {
-        base.OnHurt(source);
-        StartCoroutine(DoBlink());
-    }
     protected override void OnDeath(GameObject source)
     {
         base.OnDeath(source);
@@ -48,27 +42,6 @@ public abstract class Character : Attackable
             GameObject body = GameObject.Instantiate<GameObject>(bodyTemplate);
             body.transform.position = this.transform.position;
             body.SetActive(true);
-        }
-    }
-
-    bool blinking = false;
-    IEnumerator DoBlink()
-    {
-        if (!blinking)
-        {
-            blinking = true;
-            Color initialColor = sprite.color;
-            Color currentColor = initialColor;
-            float t = 0;
-            while (health.Invulnerable)
-            {
-                currentColor.a = ((int)((Mathf.Cos(t * 360*5f)*0.5f+0.5f)*10))/10f;
-                sprite.color = currentColor;
-                yield return new WaitForEndOfFrame();
-                t += Time.deltaTime;
-            }
-            sprite.color = initialColor;
-            blinking = false;
         }
     }
 }
