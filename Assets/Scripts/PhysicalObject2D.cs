59a3bb9f94ae551aedc51ce88d05721bf59df3ae
solroo <solroo@gmail.com>
2019-05-01 22:21:03 +0200

    started segmenting the physics

---
+++ Assets/Scripts/PhysicalObject2D.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PhysicalObject2D : MonoBehaviour
+{
+    private static readonly int MinGroundedTime = 5;
+    
+    [SerializeField] Rigidbody2D rb2D;
+    [SerializeField] Vector2 size = Vector2.one;
+    
+    [Header("State flags")]
+    // these flags are serialized only for debug reasons
+    [SerializeField] bool contactDown = false;
+    [SerializeField] bool contactUp = false;
+    [SerializeField] bool contactLeft = false;
+    [SerializeField] bool contactRight = false;
+
+    private int groundedFilter = 0;
+    private int contactCount;
+    private ContactPoint2D[] contacts = new ContactPoint2D[32];
+
+    private void Start()
+    {
+        if(!rb2D)rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void LateUpdate()
+    {
+        CheckContacts();
+    }
+
+    void CheckContacts()
+    {
+        contactDown = false;
+        contactUp = false;
+        contactLeft = false;
+        contactRight = false;
+
+        contactCount = rb2D.GetContacts(contacts);
+        for (int c = 0; c < contactCount; ++c)
+        {
+            if (contacts[c].collider.gameObject != this.gameObject && !contacts[c].collider.isTrigger)
+            {
+                if (contacts[c].point.y < transform.position.y - size.y / 2 + 0.01f)
+                {
+                    contactDown = true;
+                }
+                else if (contacts[c].point.y > transform.position.y + size.y / 2 - 0.01f)
+                {
+                    contactUp = true;
+                }
+                else if (contacts[c].point.x > transform.position.x)
+                {
+                    contactRight = true;
+                }
+                else if (contacts[c].point.x < transform.position.x)
+                {
+                    contactLeft = true;
+                }
+            }
+        }
+    }
+    public bool IsGrounded()
+    {
+        bool contact = (contactDown || (contactLeft && contactRight));
+        if (contact)
+            groundedFilter = MinGroundedTime;
+        else
+            groundedFilter--;
+        return groundedFilter > 0;
+    }
+    public bool IsBlockedX(int direction)
+    {
+        if (direction == 1) return contactRight;
+        else if (direction == -1) return contactLeft;
+        else return false;
+    }
+    public bool IsBlockedY(int direction)
+    {
+        if (direction == 1) return contactUp;
+        else if (direction == -1) return contactDown;
+        else return false;
+    }
+}
