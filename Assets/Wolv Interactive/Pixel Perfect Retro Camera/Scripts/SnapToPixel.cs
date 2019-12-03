3e0ab6c9af359d126a96949baf311c897770d4dd
solroo <solroo@gmail.com>
2019-12-03 15:47:43 +0100

    pixelperfect

---
+++ Assets/Wolv Interactive/Pixel Perfect Retro Camera/Scripts/SnapToPixel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SnapToPixel : MonoBehaviour {
+    public float z = 10;
+	private PixelCamera cam;
+	
+	float d;
+	
+	void Start() {
+		cam = GetComponentInChildren<PixelCamera>();
+		
+		d = 1f / cam.pixelsPerUnit;
+	}
+
+	void LateUpdate() {
+		Vector3 pos = transform.position;
+		Vector3 camPos = new Vector3 (pos.x - pos.x % d, pos.y - pos.y % d, pos.z+ z);	
+		cam.transform.position = camPos;
+	}
+}
